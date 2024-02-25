bebea71534852ebd042c32e1005a7c5233e7582e
Gautam <gauta.m.endiratta@gmial.com>
2024-02-25 20:45:22 +0530

    New Changes

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 console.log("faulty-calculator");
 
-/*
-calculator does following: 
-
-1. It takes two number as input from users.
-2. It performs wrong operations:-
-
-+ ----->> -
-* ----->> +
-- ----->> /
-/ ----->> **
-
-and it perform wrong operations 10% of times
-*/
 
 let first_number = prompt("Enter your first number");
 let second_number = prompt("Enter your second number");
